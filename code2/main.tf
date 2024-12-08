5e5c67a025d091c5b967c19f1237bfbf1c1397ca
john <wetindij@gmail.com>
2024-12-08 18:54:29 -0500

    ticket number

---
+++ code2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_lightsail_instance" "l1" {
+  name              = "myserver"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  key_pair_name     = "today"
+  user_data         = file ("setup.sh")
+  bundle_id         = "nano_3_0"
+  tags = {
+    env  = "dev"
+    team = "devops"
+  }
+
+}
